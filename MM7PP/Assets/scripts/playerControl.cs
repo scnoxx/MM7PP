9557ce3b521587fd8afe1663a4049f69445b13b8
SquidBoi27 <derpyhat12@gmail.com>
2025-07-20 23:54:03 -0400

    messed with the movement

--- MM7PP/Assets/scripts/playerControl.cs
+++ MM7PP/Assets/scripts/playerControl.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 public class playerControl : MonoBehaviour
 {
     InputAction moveAction;
     InputAction jumpAction;
+    InputAction sprint;
     Rigidbody rb;
     public int jumpForce = 400;
     public int moveSpeed = 10;
+    public int sprintSpeed = 2;
+    public int maxSpeed = 30;
+    
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -14,15 +19,15 @@ public class playerControl : MonoBehaviour
     {
         moveAction = InputSystem.actions.FindAction("Move");
         jumpAction = InputSystem.actions.FindAction("Jump");
+        sprint = InputSystem.actions.FindAction("Sprint");
         rb = GetComponent<Rigidbody>();
+        rb.maxLinearVelocity = maxSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveValue = moveAction.ReadValue<Vector2>();
-
-        rb.AddForce(moveValue.x * moveSpeed, 0, moveValue.y * moveSpeed);
+        Move();
 
         Jump();
     }
@@ -30,8 +35,16 @@ public class playerControl : MonoBehaviour
     {
         if (jumpAction.WasPressedThisFrame())
         {
+            rb.linearVelocity = new Vector3 (rb.linearVelocity.x, 0, rb.linearVelocity.z);
             rb.AddForce(Vector3.up * jumpForce);
         }
     }
     
+    private void Move()
+    {
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        rb.AddForce(moveValue.x * moveSpeed, 0, moveValue.y * moveSpeed);
+    }
+   
+
 }
